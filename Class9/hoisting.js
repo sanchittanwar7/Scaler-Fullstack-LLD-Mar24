47ec4ff6316ed8626f3c838baf1a65dc6572ec92
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2024-04-25 23:06:32 +0530

    hoisting

---
+++ Class9/hoisting.js
@@ -0,0 +1,14 @@
+console.log(a)
+
+var ans1 = square(a)
+var ans2 = square(20)
+
+console.log(ans1)
+console.log(ans2)
+
+var a = 10
+
+function square(a) {
+    var ans = a*a
+    return ans
+}
---
+++ Class9/tdz.js
@@ -0,0 +1,36 @@
+// console.log(a)
+
+// let ans1 = square(a)
+// let ans2 = square(20)
+
+// console.log(ans1)
+// console.log(ans2)
+
+let a = 10
+
+const square = function(a) {
+    let ans = a*a
+    return ans
+}
+
+let ans1 = square(a)
+let ans2 = square(20)
+
+console.log(ans1)
+console.log(ans2)
+
+// Redeclaration
+// let g = 45
+// let g = 50
+
+
+// Reassign
+let h = 90
+h = 50
+
+
+console.log(a)
+
+// const a = 10
+// a = 20
+// console.log(a)
