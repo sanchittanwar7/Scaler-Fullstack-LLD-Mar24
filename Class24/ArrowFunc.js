a06e156c38b5c131f54fcf48f887912bca083b7f
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2024-05-30 21:57:57 +0530

    this keyword

---
+++ Class24/ArrowFunc.js
@@ -0,0 +1,34 @@
+// "use strict"
+
+// const obj = {
+//     name: 'name',
+//     func: function() {
+//         console.log(this)
+//     }
+// }
+
+
+// obj.func()
+
+// let f = obj.func
+// f()
+
+
+// const obj = {
+//     name: 'name',
+//     func: () => {
+//         console.log(this)
+//     }
+// }
+
+// obj.func()
+
+// let f = obj.func
+// f()
+
+
+let a = 10
+a = 20
+
+const b = 10
+b = 20
---
+++ Class24/NodeJsNonStrict.js
@@ -0,0 +1,34 @@
+// Case 1: Global Context
+// console.log("GLobal context | THIS : ", this)
+
+// Case 2: Function context
+// function func() {
+//     console.log("Function context | THIS : ", this)
+// }
+// func()
+
+// Case 3: Object method
+// const obj = {
+//     name: 'name',
+//     func: function() {
+//         console.log("Object method | THIS : ", this)
+//     }
+// }
+
+// obj.func()
+
+// Case 4: Nested method
+const obj = {
+    name: 'name',
+    func: function() {
+        function func2() {
+            console.log("Object method | THIS : ", this)
+        }
+        return func2
+    }
+}
+
+console.log(obj.func()())
+
+// const nestedFunc = obj.func()
+// nestedFunc()
