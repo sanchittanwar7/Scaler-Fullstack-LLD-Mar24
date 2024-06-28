cfa0e67817e42476458934757428331bd2fc3097
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2024-06-28 23:49:47 +0530

    imdb 3

--- movies-app/src/components/Movies.jsx
+++ movies-app/src/components/Movies.jsx
@@ -4,7 +4,7 @@ import Pagination from "./Pagination";
 import axios from "axios";
 
 const Movies = () => {
-    const [pageNo, setPageNo] = useState(1)
+  const [pageNo, setPageNo] = useState(1);
   const [movies, setMovies] = useState([
     {
       url: "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
@@ -27,41 +27,73 @@ const Movies = () => {
       title: "Movie 5",
     },
   ]);
+  const [watchlist, setWatchlist] = useState([]);
+
+  const addToWatchlist = (movie) => {
+    const updatedWatchlist = [...watchlist, movie];
+    setWatchlist(updatedWatchlist);
+    localStorage.setItem("watchlist", JSON.stringify(updatedWatchlist));
+  };
+
+  const removeFromWatchList = (movie) => {
+    const updatedWatchlist = watchlist.filter(
+      (watchlistMovie) => watchlistMovie.id !== movie.id
+    );
+    setWatchlist(updatedWatchlist);
+    localStorage.setItem("watchlist", JSON.stringify(updatedWatchlist));
+  };
+
+  console.log(watchlist);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=5a6e809a6376d537b15a7d290941d94b&language=en-US&page=${pageNo}`
+      )
+      .then((response) => {
+        setMovies(response.data.results);
+      });
+  }, [pageNo]);
 
   useEffect(() => {
-    axios.get(
-      `https://api.themoviedb.org/3/trending/movie/day?api_key=5a6e809a6376d537b15a7d290941d94b&language=en-US&page=${pageNo}`
-    ).then(response => {
-      console.log(response)
-      setMovies(response.data.results)  
-    })
-  }, [pageNo])
+    const watchlist = localStorage.getItem("watchlist");
+    if (watchlist) {
+      setWatchlist(JSON.parse(watchlist));
+    }
+  }, []);
 
   const handleNext = () => {
-    setPageNo(pageNo + 1)
-  }
+    setPageNo(pageNo + 1);
+  };
 
   const handlePrevious = () => {
     if (pageNo > 1) {
-        setPageNo(pageNo - 1)
+      setPageNo(pageNo - 1);
     }
-  }
+  };
+
   return (
-  <div>
-    <div className="text-2xl font-bold text-center m-5">
+    <div>
+      <div className="text-2xl font-bold text-center m-5">
         <h1>Trending Movies</h1>
+      </div>
+      <div className="flex justify-evenly flex-wrap gap-8">
+        {movies.map((movie) => {
+          return (
+            <MovieCard
+              movie={movie}
+              addToWatchlist={addToWatchlist}
+              removeFromWatchList={removeFromWatchList}
+              watchlist={watchlist}
+            />
+          );
+        })}
+      </div>
+      <Pagination
+        pageNo={pageNo}
+        nextPageFn={handleNext}
+        previousPageFn={handlePrevious}
+      />
     </div>
-    <div className="flex justify-evenly flex-wrap gap-8">
-        {
-            movies.map(movie => {
-                return (
-                    <MovieCard movie={movie} />
-                )
-            })
-        }
-    </div>
-    <Pagination pageNo={pageNo} nextPageFn={handleNext} previousPageFn={handlePrevious}/>
-  </div>
   );
 };
 
--- movies-app/src/components/Watchlist.jsx
+++ movies-app/src/components/Watchlist.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Watchlist = () => {
-  return (
-    <div>Watchlist</div>
-  )
-}
+  const [watchlist, setWatchlist] = useState([]);
+  const genreids = {
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    14: "Fantasy",
+    36: "History",
+    27: "Horror",
+    10402: "Music",
+    9648: "Mystery",
+    10749: "Romance",
+    878: "Sci-Fi",
+    10770: "TV",
+    53: "Thriller",
+    10752: "War",
+    37: "Western",
+  };
 
-export default Watchlist
+  useEffect(() => {
+    const watchlist = localStorage.getItem("watchlist");
+    if (watchlist) {
+      setWatchlist(JSON.parse(watchlist));
+    }
+  }, []);
+  return (
+    <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+      <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+        <thead>
+          <tr className="bg-gray-50">
+            <th className="px-6 py-4 font-medium text-gray-900">Name</th>
+            <th>
+              <div className="flex">
+                <div>Ratings</div>
+              </div>
+            </th>
+            <th>
+              <div className="flex">
+                <div>Popularity</div>
+              </div>
+            </th>
+            <th>
+              <div className="flex">
+                <div>Genre</div>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+          {watchlist.map((movie) => (
+            <tr className="hover:bg-gray-50">
+              <td className="flex items-center px-6 py-4 font-normal text-gray-900">
+                <img className="h-[6rem] w-[10rem] object-fit" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="" />
+                <div className="font-medium text-gray-700 text-sm">
+                  {movie.title}
+                </div>
+              </td>
+              <td className="pl-6 py-4">{movie.vote_average}</td>
+              <td className="pl-6 py-4">{movie.popularity}</td>
+              <td className="pl-2 py-4">{movie.genre_ids}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Watchlist;
